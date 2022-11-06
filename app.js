6b3e90f643e2d1c7c21b6cdc865257289fb78d3a
Natonya Stewart <91899843+natonyastewart@users.noreply.github.com>
2022-11-06 09:25:19 -0500

    Initial JS commit

---
+++ app.js
@@ -0,0 +1,19 @@
+window.onload = function() { 
+
+    var search = document.getElementById("btn");
+    search.addEventListener("click", function(e) {
+        e.preventDefault();
+
+
+    var result = document.getElementById("result");
+    var Input = document.getElementById("SuperHeroes").value.replace(/[-&\/\\#,+()$@|~%!.'":;*?<>{}]/g,'');
+    
+    fetch(`http://localhost:8888/info2180-lab4/superheroes.php?query= ${Input}`, {method : 'GET'})
+    .then(response => response.text())
+    .then(data => {
+        result.innerHTML = "";
+        result.innerHTML = data; 
+    })
+    .catch(error => console.log("There was a problem with the request"));
+  });
+};
